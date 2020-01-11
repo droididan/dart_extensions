de4fdc6eb720d1bd72617f124f178c300b9ed4be
Idan Ayalon <idan@l.com>
2020-01-11 11:25:18 +0200

    copyright

--- lib/date.dart
+++ lib/date.dart
@@ -1,5 +1,19 @@
-extension DateExt on DateTime {
+/*
+ * Copyright 2020 Idan Ayalon. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import 'package:flutter/cupertino.dart';
+
+extension DateExt on DateTime {
   /// to add years to a [DateTime] add a positive number
   /// to remove years pass a negative number
   addOrRemoveYears(int years) {
@@ -29,4 +43,11 @@ extension DateExt on DateTime {
   addOrRemoveSeconds(int sec) {
     return DateTime(this.year, this.month, this.day, this.minute, this.second + sec);
   }
+}
+
+main() {
+  final set = Set.from([1,2,3,4]);
+
+
+
 }
